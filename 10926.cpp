c19a3fdd3406fc7a5e374c70de68af3433659478
ehwlfk2 <ehwlfk2@gmail.com>
2022-01-29 12:17:50 +0900

    22,01.29 기본 입출력 문제 추가

---
+++ 10926.cpp
@@ -0,0 +1,10 @@
+#include<cstdio>
+#pragma warning(disable:4996)
+
+int main(void) {
+	char p[51];
+	scanf("%s", &p);
+	printf("%s??!", p);
+
+	return 0;
+}
---
+++ 18108.cpp
@@ -0,0 +1,8 @@
+#include<cstdio>
+#pragma warning(disable:4996)
+int main(void) {
+	int n;
+	scanf("%d", &n);
+	printf("%d", n - 543);
+	return 0;
+}
